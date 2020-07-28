291e537e7c38e38da83018b7541da399c5b9daec
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-27 22:41:41 -0300

    Create programa3.py

---
+++ Aula_1/programa3.py
@@ -0,0 +1,22 @@
+#entrada de dados
+#função input()
+
+#programa soma
+a=int(input("Digite um número inteiro: "))
+b=int(input("Digite outro número inteiro: "))
+#int() -> variável inteira
+soma = a + b
+
+print("A soma é: ", soma)
+
+# + -> junta as strings
+
+#programa produto
+
+a= float (input ("Digite um número: "))
+b= float (input("Digite um número: "))
+
+# float() -> ponto flutuante -> números decimais
+produto = a*b
+
+print("O produto é: ", produto)
