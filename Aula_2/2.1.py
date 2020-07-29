40ef43be8c95c053419fb13d6ec736b7b1496fca
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-29 20:17:09 -0300

    Create 2.1.py

---
+++ Aula_2/2.1.py
@@ -0,0 +1,8 @@
+#Condicional
+#-Simples -> if -- if só roda uma vez
+idade=int(input("idade do carro: \n"))
+
+if idade <=3:
+                print("caranga nova")
+if idade >3:
+                print("caranga velha")
