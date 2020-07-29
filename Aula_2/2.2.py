5999126926a37d2099e148a097b80d3fb1ae7a23
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-29 20:17:59 -0300

    Create 2.2.py

---
+++ Aula_2/2.2.py
@@ -0,0 +1,10 @@
+# Maior ou menor
+print("Decidir se é >= ou <= ou =")
+número1=float(input("Entre com um número: \n"))
+número2=float(input("Entre com um número: \n"))
+if número1>número2:
+                    print("O primeiro número é o maior")
+if número1<número2:
+                    print("O primeiro número é o menor")
+if número1==número2:
+                    print("Os dois números são iguais")
