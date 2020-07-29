79d974f954262874001e39488ba805dbaa00935c
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-29 20:18:23 -0300

    Create 2.3.py

---
+++ Aula_2/2.3.py
@@ -0,0 +1,7 @@
+#Composto -> if else
+
+idade=int(input("idade do carro: \n"))
+if idade <=3:
+                print("caranga nova") # comando do True
+else:
+                print("caranga velha") # comando do False
