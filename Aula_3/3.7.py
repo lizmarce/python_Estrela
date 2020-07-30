4a70b1e66f7891083854090e88b2d3efe6e930b3
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-30 18:41:07 -0300

    Add files via upload

---
+++ Aula_3/3.7.py
@@ -0,0 +1,19 @@
+#lista -> armazenar -> 0,1,2,...,n elementos
+#elementos -> qualquer tipo -> inclusive lista
+
+lista=[1,3,5]  # declaro lista
+print("lista=", lista)
+print("tamanho",len(lista)) # tamanho
+print("tipo",type(lista)) # tipo
+print("primeiro elemento=",lista[0]) # primeiro elemento
+print("segundo elemento=",lista[1]) # segundo elemento
+print("terceiro elemento=",lista[2]) # terceiro elemento
+
+
+lista2=[lista,"a", 5]  # declaro lista
+print("lista2=", lista2)
+print("tamanho",len(lista2)) # tamanho
+print("tipo",type(lista2)) # tipo
+print("primeiro elemento=",lista2[0]) # primeiro elemento
+print("segundo elemento=",lista2[1]) # segundo elemento
+print("terceiro elemento=",lista2[2]) # terceiro elemento
