a6f5b592969938b4d14a2ed5e84bfe5860fc3fb7
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-30 18:40:30 -0300

    Create 3.5.py

---
+++ Aula_3/3.5.py
@@ -0,0 +1,16 @@
+#interrompendo a repetição
+
+soma=0
+while True:
+                    número=int(input("Digite um número a somar ou zero para sair:"))
+                    if número==0:
+                                        break
+                    soma=soma+número
+print(soma)
+
+#while <condição>
+                #comando True
+#while True
+
+
+#<condição> ->valor lógico -> True or False
