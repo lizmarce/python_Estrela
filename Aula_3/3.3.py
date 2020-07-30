0b82bcbc3f0fce67d77935ad9971ac8b345cd057
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-30 18:39:31 -0300

    Create 3.3.py

---
+++ Aula_3/3.3.py
@@ -0,0 +1,11 @@
+#Acumuladores
+#variáveis que recebem ela mais algo não constante
+#por exemplo soma=soma+k
+
+n=1
+soma=0
+while n<=5:
+                    x=int(input(f"Digite o {n} número: "))
+                    soma=soma+x
+                    n=n+1
+print(f"Soma: {soma}")
