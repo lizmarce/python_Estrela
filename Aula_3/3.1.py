9fb94015545f7c8c6ddf6ce2603f358f25792c88
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-30 18:38:42 -0300

    Create 3.1.py

---
+++ Aula_3/3.1.py
@@ -0,0 +1,13 @@
+#Repetição
+
+#while < condição>:
+#               bloco True
+
+# if < condição>:
+#               bloco True
+
+n=1 # inicializar a variável
+fim=int(input("Digite o último número:"))
+while n<=fim:
+                print(n)
+                n=n+1
