e599994db8aa2ff5192e7d5330be4cee39758f5f
lizmarce <61739388+lizmarce@users.noreply.github.com>
2020-07-30 18:40:47 -0300

    Create 3.6.py

---
+++ Aula_3/3.6.py
@@ -0,0 +1,5 @@
+caracter="S"
+while caracter=="S" or caracter=="s":
+                    nome=input("Digite seu nome: \n")
+                    print (f"Boa noite {nome}")
+                    caracter=input("Digite S para repetir")
